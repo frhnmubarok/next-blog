493a39ce62d1d3217723b5b44ab2a76837c2ade4
frhnmubarok <farhanmubarok104@gmail.com>
2022-01-12 16:11:17 +0700

    fix: remove text for dark mode test

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,15 +10,14 @@ const Header: React.FC = () => {
         <a className='text-2xl font-bold text-gray-700'>
           Farhan&apos;s
           <span className='ml-2 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-cyan-500 relative inline-block'>
-            <span className='relative text-white'>Blog</span>
+            <span className='relative text-white dark:text-gray-300'>Blog</span>
           </span>
-          The current theme is: {theme}
         </a>
       </Link>
       <button
         aria-label='Toggle Dark Mode'
         type='button'
-        className='p-3 h-12 w-12 order-2 md:order-3'
+        className='p-3 h-12 w-12 order-2 md:order-3 hidden'
         onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
         Toggle
       </button>
